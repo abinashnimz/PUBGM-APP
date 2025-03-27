53c49aac5d68bfadb10f7e4280afcaf85d8a59ff
abinashnimz <abinash443steam@gmail.com>
2025-03-27 18:50:06 +0530

    added user routes

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,15 @@
+export const getSingleUser = ()=>{
+    console.log('single user get working');
+}
+
+export const getAllUsers = ()=>{
+    console.log('getAllUsers working');
+}
+
+export const deleteUser = ()=>{
+    console.log('deleteUser working');
+}
+
+export const editUser = ()=>{
+    console.log('editUser working');
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import {signIn, signOut} from '../controllers/user.controller.js';
+import {
+    getSingleUser,
+    getAllUsers,
+    deleteUser,
+    editUser
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 
 //All the User routes
-router.route('/getAll').get(getAllUsers);
 router.route('/:userId').get(getSingleUser);
+router.route('/getAll').get(getAllUsers);
 router.route('/delete/:userId').delete(deleteUser);
 router.route('/update/:userId').put(editUser);
 
